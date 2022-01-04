b4c95a57dd01ba341e78d5b0b6340d0f96654586
dmitrij <turenkodm@gmail.com>
2022-01-04 11:17:42 +0300

    Register app

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:ui';
+
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
@@ -58,13 +60,16 @@ class MyApp extends StatelessWidget {
             const SizedBox(
               height: 7.0,
             ),
-            Text(
-              'Чтобы зарегистрироваться введите свой номер телефона и почту',
-              textAlign: TextAlign.center,
-              style: GoogleFonts.roboto(
-                fontSize: 16,
-                fontWeight: FontWeight.w400,
-                color: const Color(0xFFC4C4C4),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 10.0),
+              child: Text(
+                'Чтобы зарегистрироваться введите свой номер телефона и почту',
+                textAlign: TextAlign.center,
+                style: GoogleFonts.roboto(
+                  fontSize: 16,
+                  fontWeight: FontWeight.w400,
+                  color: const Color(0xFFC4C4C4),
+                ),
               ),
             ),
             const SizedBox(
@@ -75,7 +80,6 @@ class MyApp extends StatelessWidget {
               textAlign: TextAlign.center,
               style: GoogleFonts.roboto(
                 fontSize: 16,
-                fontWeight: FontWeight.w400,
               ),
             ),
             const SizedBox(
@@ -83,16 +87,97 @@ class MyApp extends StatelessWidget {
             ),
             Padding(
               padding: const EdgeInsets.fromLTRB(38.0, 5, 38, 0),
-              child: TextField(
-                decoration: InputDecoration(
-                  hintText: '+7',
-                  filled: true,
-                  fillColor: const Color(0xFFECEFF1),
-                  focusedBorder: borderStyle,
-                  enabledBorder: borderStyle,
+              child: SizedBox(
+                height: 34,
+                child: TextField(
+                  style: GoogleFonts.roboto(fontSize: 16),
+                  decoration: InputDecoration(
+                    contentPadding: const EdgeInsets.fromLTRB(12, 8, 0, 0),
+                    hintText: '+7',
+                    filled: true,
+                    fillColor: const Color(0xFFECEFF1),
+                    focusedBorder: borderStyle,
+                    enabledBorder: borderStyle,
+                  ),
                 ),
               ),
             ),
+            const SizedBox(
+              height: 14.0,
+            ),
+            Text(
+              'Почта',
+              textAlign: TextAlign.center,
+              style: GoogleFonts.roboto(
+                fontSize: 16,
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.fromLTRB(38.0, 5, 38, 0),
+              child: SizedBox(
+                height: 34,
+                child: TextField(
+                  style: GoogleFonts.roboto(fontSize: 16),
+                  decoration: InputDecoration(
+                    contentPadding: const EdgeInsets.fromLTRB(12, 8, 0, 0),
+                    filled: true,
+                    fillColor: const Color(0xFFECEFF1),
+                    focusedBorder: borderStyle,
+                    enabledBorder: borderStyle,
+                  ),
+                ),
+              ),
+            ),
+            const SizedBox(
+              height: 24.0,
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 10.0),
+              child: Text(
+                'Вам придет четырехзначный код, который будет вашим паролем',
+                textAlign: TextAlign.center,
+                style: GoogleFonts.roboto(
+                  fontSize: 16,
+                  fontWeight: FontWeight.w400,
+                  color: const Color(0xFFC4C4C4),
+                ),
+              ),
+            ),
+            const SizedBox(
+              height: 14.0,
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 34.0),
+              child: Text(
+                  'Изменить пароль можно будет в личном кабинете после регистрации',
+                  textAlign: TextAlign.center,
+                  style: GoogleFonts.roboto(
+                    wordSpacing: 5,
+                    fontSize: 16,
+                    fontWeight: FontWeight.w400,
+                    color: const Color(0xFFC4C4C4),
+                  )),
+            ),
+            const SizedBox(
+              height: 43.0,
+            ),
+            SizedBox(
+              height: 42,
+              width: 154,
+              child: ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                    primary: const Color(0xFF0079D0),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(22),
+                    ),
+                    textStyle: GoogleFonts.roboto(
+                      fontSize: 16,
+                      fontWeight: FontWeight.w500,
+                    )),
+                onPressed: () {},
+                child: Text('Отправить код'),
+              ),
+            )
           ],
         ),
       ),
